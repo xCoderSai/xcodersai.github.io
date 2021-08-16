e766c8fb0458dbf7f8fceeb2c1132b4879af1f05
Catherine De Mesa <59064455+xCoderSai@users.noreply.github.com>
2021-08-16 10:39:47 -0500

    Update swag.js

--- swag.js
+++ swag.js
@@ -1,4 +1,42 @@
-$( document ).ready(function() {
-    $("#sortable").sortable();
-    $("#sortable").disableSelection();
+mobiscroll.settings = {
+    theme: 'ios',
+    themeVariant: 'light'
+};
+
+$(function () {
+
+    $('#card-deck').mobiscroll().listview({
+        stages: [{
+            percent: -20,
+            action: function (event, inst) {
+                inst.remove(event.target);
+                return false;
+            }
+        }, {
+            percent: 20,
+            action: function (event, inst) {
+                inst.remove(event.target);
+                return false;
+            }
+        }],
+        actionable: false
+    });
+
+    $('#card-deck-cycle').mobiscroll().listview({
+        stages: [{
+            percent: -20,
+            action: function (event, inst) {
+                inst.move(event.target, 0);
+                return false;
+            }
+        }, {
+            percent: 20,
+            action: function (event, inst) {
+                inst.move(event.target, 0);
+                return false;
+            }
+        }],
+        actionable: false
+    });
+
 });
